8bb5d05468179bdc4925b7b875693c48af84e227
Wentao Xu <wxu22@ucsc.edu>
2022-05-30 19:33:31 -0400

    readme

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -4,12 +4,12 @@
 ***REMOVED***
 
 ***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
+    apiKey: "AIzaSyDNAQa31fGYKH6g02jo1MJnub4usiu8R_A",
+    authDomain: "kaomoji-54124.firebaseapp.com",
+    projectId: "kaomoji-54124",
+    storageBucket: "kaomoji-54124.appspot.com",
+    messagingSenderId: "123637734263",
+    appId: "1:123637734263:web:4113cb31a4cacf83aadf72"
 ***REMOVED***
 
 ***REMOVED***
