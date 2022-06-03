77e46d747d1883fc34d826d12954e28f6c68c6d5
Wentao Xu <wxu22@ucsc.edu>
2022-06-03 12:43:06 -0400

    upload script

--- src/components/upload.js
+++ src/components/upload.js
@@ -11,71 +11,71 @@
 // const data = require("../../helper_scripts/kaomoji-2.json")
 
 /* Copy code below to App.js to populate Firestore Emulator */
-// const data = require("../kaomoji-2.json")
+const data = require("../kaomoji-2.json")
 
-//   function isCollection( data, path, depth ) {
-//     if( 
-//         typeof data != 'object' ||
-//         data == null ||
-//         data.length === 0 ||
-//         isEmpty(data)
-//     ){
-//         return false
-//     }
+  function isCollection( data, path, depth ) {
+    if( 
+        typeof data != 'object' ||
+        data == null ||
+        data.length === 0 ||
+        isEmpty(data)
+    ){
+        return false
+    }
 
-//     for( const key in data ){
-//         if( typeof data[key] != 'object' || data[key] == null ){
-//             return false
-//         }
-//     }
+    for( const key in data ){
+        if( typeof data[key] != 'object' || data[key] == null ){
+            return false
+        }
+    }
 
-//     return true
-//   }
+    return true
+  }
 
-//   function isEmpty( obj ){
-//     for( const key in obj ){
-//         if( obj.hasOwnProperty(key) ){
-//             return false
-//         }
-//     }
-//     return true
-//   }
+  function isEmpty( obj ){
+    for( const key in obj ){
+        if( obj.hasOwnProperty(key) ){
+            return false
+        }
+    }
+    return true
+  }
 
-//   async function upload( data, path ){
-//     return await db.collection('kaomojis')
-//         .add({
-//             name: data.name,
-//             category: [data.category],
-//             likes: 0, 
-//         })
-//         .then( () => console.log(`Document ${path.join('/')} uploaded.`) )
-//         .catch( () => console.error(`Could not write document ${path.join('/')}.`))
-//     // console.log( [data.category] )
-//   }
+  async function upload( data, path ){
+    return await db.collection('kaomojis')
+        .add({
+            name: data.name,
+            category: [data.category],
+            likes: 0, 
+        })
+        .then( () => console.log(`Document ${path.join('/')} uploaded.`) )
+        .catch( () => console.error(`Could not write document ${path.join('/')}.`))
+    // console.log( [data.category] )
+  }
 
-//   async function resolve( data, path = [] ){
-//     if( path.length > 0 && path.length % 2 == 0 ){
-//         const documentData = Object.assign( {}, data )
+  async function resolve( data, path = [] ){
+    if( path.length > 0 && path.length % 2 == 0 ){
+        const documentData = Object.assign( {}, data )
 
-//         for( const key in data ){
-//             if( isCollection( data[key], [...path, key] )){
-//                 delete documentData[key]
-//                 console.log(data[key])
-//                 resolve( data[key], [...path, key] )
-//             }
-//         }
+        for( const key in data ){
+            if( isCollection( data[key], [...path, key] )){
+                delete documentData[key]
+                console.log(data[key])
+                resolve( data[key], [...path, key] )
+            }
+        }
 
-//         if( !isEmpty(documentData) ){
-//             await upload( documentData, path )
-//         }
-//     }
-//     else{
-//         for( const key in data ){
-//             await resolve( data[key], [...path, key] )
-//         }
-//     }
-//   }
-//   resolve(data)
+        if( !isEmpty(documentData) ){
+            await upload( documentData, path )
+        }
+    }
+    else{
+        for( const key in data ){
+            await resolve( data[key], [...path, key] )
+        }
+    }
+  }
+  resolve(data)
 
 /* Command to clear all data in Firestore
 // firebase firestore:delete --all-collections
